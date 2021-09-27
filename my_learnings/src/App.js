85fa471a184adadbc147ebdedb3ff7f1e782eb14
shaji <shajis001@gmail.com>
2021-09-27 18:39:09 +0530

    Added for temp

--- my_learnings/src/App.js
+++ my_learnings/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
+import NameList from './components/NameList';
 
 function App() {
   return (
-  <List/>
+    <>
+      <List/>
+  <NameList/>
+     </>
+
   )
 }
 
--- my_learnings/src/components/List.js
+++ my_learnings/src/components/List.js
@@ -1,19 +1,11 @@
-import React from 'react';
+import React from "react";
 
+const myLists = ["A", "B", "C", "D", "D"];
 
-const myLists = ['A', 'B', 'C', 'D', 'D'];
+const List = () => {
+  const numberList = myLists.map((num) => num);
 
- const List=() =>{
-  
-const numberList = myLists.map(num =>
-num
-);
-
-    return <ul>
-        {numberList}
-    </ul>
-}
-
-
+  return <ul>{numberList}</ul>;
+};
 
 export default List;
