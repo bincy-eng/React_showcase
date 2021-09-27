5d4424a87558356b2a27de6b1b6d85c8f3217460
Bincy <saleenabincy946@gmail.com>
2021-09-27 23:21:58 +0530

    datalist

--- my_learnings/src/App.js
+++ my_learnings/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
+import Namelist from './components/Namelist';
 
 function App() {
   return (
+    <>
   <List/>
+  <Namelist/>
+  </>
   )
 }
 
