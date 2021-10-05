04a9c600540842b2030ebf7df025f848f0c0954f
Bincy <saleenabincy946@gmail.com>
2021-10-05 15:58:54 +0530

    count by using destructoring

--- my_learnings/src/components/Destructoring.js
+++ my_learnings/src/components/Destructoring.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {useState} from 'react';
 
 function Destructoring(){
-    return <>hi</>
+    const [count, setCount] =useState(0)
+    return (
+        <>
+        <p>you clicked {count} times</p>
+        <button onClick={()=> setCount(count+1)}>Click Me</button>
+        </>
+    )
 }
 
 export default Destructoring;
